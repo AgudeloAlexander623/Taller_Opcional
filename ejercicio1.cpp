a5b90bcd257ff656ebd4787cc0ed14402a633ea3
Alexander Agudelo <agudeloalexander623@gmail.com>
2025-04-08 20:07:10 -0500

    first commit

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,8 +1,24 @@
+/*
+Verificar si un número es positivo o negativo. Enunciado: Pide al usuario que
+ingrese un número e imprime si es positivo, negativo o cero
+*/
+
 #include<iostream>
 #include<string.h>
 
 using namespace std;
 
 int main(){
-    
+    int a;
+
+    cout<<"ingresar un numero por favor"<<endl;
+    cin>>a;
+
+    if(a<0){
+        cout<<"el numero ingresado es negativo"<<endl;
+        cout<< a ;
+    }else{
+        cout<<"el numero ingresado es positivo"<<endl;
+        cout<<a;
+    }
 }
